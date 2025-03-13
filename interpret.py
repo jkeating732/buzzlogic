f8d5c778cfab720c5dfecd79cd4a681ed30e0ac2
Whoffie <will.hoffman@pm.me>
2025-03-13 16:59:59 -0500

    Make it easier to work with interpreter during development

--- interpret.py
+++ interpret.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
+import cv2
 from ultralytics import YOLO
+from dotenv import load_dotenv
 
 # Load a model
-model = YOLO("models/model.pt")  # pretrained model
+model = YOLO("models/model_segment.pt")  # change as needed
 
 numImages = int(input("Enter the number of images to feed the model:\n"))
 
@@ -18,14 +21,11 @@ if numImages >= 1:
 else:
     print("Please enter a whole number greater than zero")
 
-results = model(images)  # return a list of Results objects
+results = model.predict(images)  # return a list of Results objects
 
-# Process results list
 for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-    result.show()  # display to screen
-    result.save(filename="result.jpg")  # save to disk
+    plot = result.plot(labels=True, boxes=True, probs=False)
+    plot_rgb = cv2.cvtColor(plot, cv2.COLOR_BGR2RGB)
+    plt.axis("off")
+    plt.imshow(plot)
+    plt.show()
